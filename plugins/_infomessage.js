aa86a5748e55bfea3acbce5449ac1538cd2283b3
Sifu <williamspuerto02@gmail.com>
2025-11-29 18:54:03 -0600

    Update _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -16,7 +16,7 @@ const users = m.messageStubParameters[0]
 const usuario = await resolveLidToRealJid(m?.sender, conn, m?.chat)
 const groupAdmins = participants.filter(p => p.admin)
 const rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: "𐔌 . ⋮ ᗩ ᐯ I Տ O .ᐟ ֹ ₊ ꒱", body: textbot, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/k4fknt.jpg'
 
 const nombre = `📝 @${usuario.split('@')[0]} ha cambiado el *nombre del grupo*.\n📛 Ahora el grupo se llama:\n> *${m.messageStubParameters[0]}*`
 const foto = `🖼️ Se ha cambiado la *imagen del grupo*.\n👤 Acción hecha por:\n> » @${usuario.split('@')[0]}`

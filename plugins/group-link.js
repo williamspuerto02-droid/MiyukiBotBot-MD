4aa22b247b3e0e83d95214f043f0feba5c035855
Sifu <williamspuerto02@gmail.com>
2025-11-29 19:34:02 -0600

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, args }) => {
 let group = m.chat
-const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/ucmzlc.jpg')
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
 await conn.sendMessage(group, { image: { url: pp }, caption: message })
